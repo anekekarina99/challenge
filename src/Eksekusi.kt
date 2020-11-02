007b2410473d4df46b14a6328148b6f484fc85e4
unknown <anekekarina99@gmail.com>
2020-11-02 11:01:05 +0700

    test

--- src/Eksekusi.kt
+++ src/Eksekusi.kt
@@ -1,2 +1,47 @@
-class Eksekusi {
+class Eksekusi ( pilihan1 : String?,  pilihan2: String?) {
+
+    private val pilihan1: String? = pilihan1
+    private val pilihan2: String? = pilihan2
+
+
+    fun eksekusiHasil() {
+        if ((pilihan1 == "Batu") &&( pilihan2 == "Batu"))
+        {
+            println( "Seri")
+        }else if ((pilihan1 == "Batu")&&( pilihan2 == "Gunting"))
+        {
+           println("Player 1 Menang")
+        }
+        else if ((pilihan1 == "Batu")&&( pilihan2 == "Kertas"))
+        {
+            println( "Player 2 Menang")
+        }
+        else if ((pilihan1 == "Kertas")&&(pilihan2 == "Kertas"))
+        {
+            println( "Seri")
+        }
+        else if ((pilihan1 == "Kertas")&& (pilihan2 == "Batu"))
+        {
+            println("Player 1 Menang")
+        }
+        else if ((pilihan1 == "Kertas")&&(pilihan2 == "Gunting"))
+        {
+            println("Player 2 Menang")
+        }
+        else if ((pilihan1 == "Gunting")&&(pilihan2 == "Gunting"))
+        {
+            println( "Seri")
+        }
+        else if ((pilihan1 == "Gunting")&&(pilihan2 == "Kertas"))
+        {
+            println( "Player 1 Menang")
+        }
+        else if ((pilihan1 == "Gunting")&& (pilihan2 == "Batu")){
+            println( "Player 2 Menang")
+        }
+        else{
+            println( "error")
+        }
+
+    }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,14 @@
-fun main(args: Array<String>){
-    println ("Selamat datang, permainan batu gunting kertas, pilih (Batu),(Gunting) atau (Kertas)")
-    println("Masukan Pilihan 1 :")
-    val pilihan1 : String? = readLine()
-    println("Masukan Pilihan 2 :")
-    val pilihan2 : String? = readLine()
-    //*eksekusi
-    val eksekusi = Eksekusi(pilihan1,pilihan2)
-    var hasil = eksekusi.eksekusiHasil()
-    //print
-    println(hasil)
-}
+
+    fun main(args: Array<String>) {
+        println("Selamat datang, permainan batu gunting kertas, pilih (Batu),(Gunting) atau (Kertas)")
+        println("Masukan Pilihan 1 :")
+        val pilihan1: String? = readLine()
+        println("Masukan Pilihan 2 :")
+        val pilihan2: String? = readLine()
+        //*eksekusi
+        val eksekusi = Eksekusi(pilihan1, pilihan2)
+        eksekusi.eksekusiHasil()
+
+
+    }
+//catatan tidak memakai mvc
